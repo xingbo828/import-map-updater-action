b4990b6a9e74c65db43416ec3f2e3fc16cc2f6f7
Bo <xingbo828@gmail.com>
2022-05-16 14:30:35 -0700

    My first action is ready

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const core = require('@actions/core')
-const { HttpClient  } = require('@actions/http-client')
-const { BasicCredentialHandler } = require('@actions/http-client/lib/auth')
+const core = require('@actions/core');
+const { HttpClient } = require('@actions/http-client');
+const { BasicCredentialHandler } = require('@actions/http-client/lib/auth');
 
-try {
-  const auth = new BasicCredentialHandler(
-    core.getInput('imd-username'), core.getInput('imd-password')
-  )
-  const client = new HttpClient('import-map-deployer', [auth])
-  const patchUrl = `${core.getInput('imd-server')}/service?env=${core.getInput('imd-env')}`
-  const response = await client.patchJson(patchUrl, {
-    service: core.getInput('service'),
-    url: core.getInput('url')
-  })
-  console.log(`response:, ${JSON.stringify(response)}`)
-  core.setOutput('status', response.status)
-} catch (error) {
-  core.setFailed(error.message)
-}
+const exec = async () => {
+  try {
+    const auth = new BasicCredentialHandler(
+      core.getInput('imd-username'),
+      core.getInput('imd-password')
+    );
+    const client = new HttpClient('import-map-deployer', [auth]);
+    const patchUrl = `${core.getInput(
+      'imd-server'
+    )}/service?env=${core.getInput('imd-env')}`;
+    const response = await client.patchJson(patchUrl, {
+      service: core.getInput('service'),
+      url: core.getInput('url'),
+    });
+    console.log(`response:, ${JSON.stringify(response)}`);
+    core.setOutput('status', response.status);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+exec();
